f743c321b4fb81c74787323e515d830f67789b17
laxmikant <45011194+LaxmikantRaikar@users.noreply.github.com>
2020-10-06 15:42:12 +0530

    bug fixes

--- fwrs.py
+++ fwrs.py
@@ -8,7 +8,7 @@ from qgis.utils import iface
 
 
 class ForRev:
-    
+
 
     def __init__(self, iface):
         self.iface = iface
@@ -29,7 +29,7 @@ class ForRev:
         self.toolbar.addAction(self.farAction)
         self.iface.addPluginToMenu("Forward reverse Toolbar", self.farAction)
 
-        
+
 
         icon = QIcon(os.path.dirname(__file__) + "/images/rev.png")
         self.revAction = QAction(icon, "Reverse one feature", self.iface.mainWindow())
@@ -43,7 +43,7 @@ class ForRev:
         self.iface.addPluginToMenu("Forward reverse Toolbar", self.revAction)
 
 
-        icon = QIcon(os.path.dirname(__file__) + "/images/for.png")
+        icon = QIcon(os.path.dirname(__file__) + "/images/sett.png")
         self.settAction = QAction(icon, "Settings", self.iface.mainWindow())
         self.settAction.setObjectName('settings')
         self.settAction.triggered.connect(self.sett)
@@ -51,7 +51,7 @@ class ForRev:
         self.toolbar.addAction(self.settAction)
         self.iface.addPluginToMenu("Forward reverse Toolbar", self.settAction)
 
-     
+
 
 
     def unload(self):
@@ -65,60 +65,60 @@ class ForRev:
         self.iface.removeToolBarIcon(self.farAction)
         self.iface.removeToolBarIcon(self.revAction)
         self.iface.removeToolBarIcon(self.settAction)
-        
-        
+
+
         self.iface.unregisterMainWindowAction(self.farAction)
         self.iface.unregisterMainWindowAction(self.revAction)
-        
+
         del self.toolbar
 
     def sett(self):
         self.iface.messageBar().pushMessage("Settings add")
 
-        
+
     def forward(self):
-        try:
-            layer = iface.activeLayer()
-            count = layer.selectedFeatureCount()
-            selection = layer.selectedFeatures()
+        layer = iface.activeLayer()
+        count = layer.selectedFeatureCount()
+        selection = layer.selectedFeatures()
+
+    try:
+        if count == 1:
+            for feature in selection:
+                cur_id = feature.id()
+                print(cur_id)
+                cur_id = cur_id+1
+                layer.removeSelection()
+                layer.select(cur_id)
+                iface.actionZoomToSelected().trigger()
 
 
-
-            if count == 1:
-                for feature in selection:
-                    try:
-                        layer.removeSelection()
-                        layer.select(feature['_id_']+1)
-                        iface.actionZoomToSelected().trigger();
-                        print(feature['_id_']+1)
-
-                    except:
-                        self.iface.messageBar().pushMessage("First feature reached")
-
-
-            else:
+        else:
                 self.iface.messageBar().pushMessage("Select one vector feature")
-        except:
-            self.iface.messageBar().pushMessage("Select one vector feature")
+    except:
+            self.iface.messageBar().pushMessage("Reached First object")
+
 
 
     def reverse(self):
+        layer = iface.activeLayer()
+        count = layer.selectedFeatureCount()
+        selection = layer.selectedFeatures()
         try:
-            layer = iface.activeLayer()
-            count = layer.selectedFeatureCount()
-            selection = layer.selectedFeatures()
             if count == 1:
                 for feature in selection:
-                    try:
-                        layer.removeSelection()
-                        layer.select(feature['id']-1)
-                        iface.actionZoomToSelected().trigger();
-                        print(feature['id']-1)
-                    except:
-                        print("Last Feature reached")
+
+                    cur_id = feature.id()
+                    print(cur_id)
+                    cur_id = cur_id-1
+                    layer.removeSelection()
+
+                    layer.select(cur_id)
+                    iface.actionZoomToSelected().trigger()
+
+
             else:
                 self.iface.messageBar().pushMessage("Select one vector feature")
         except:
-            self.iface.messageBar().pushMessage("Select one vector feature")
+            self.iface.messageBar().pushMessage("Reached Last object")
 
 
